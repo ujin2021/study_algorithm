745c2731fb9b5b0f1f20fb96d8ed31876e45a5f9
ujin2021 <ujin2021@naver.com>
2020-08-31 18:04:13 +0900

    p, y 찾기

---
+++ programmers-python/level1/solution11.py
@@ -0,0 +1,6 @@
+# p, y 찾기
+def solution(s):
+    # if (s.lower().count('p') == s.lower().count('y')) :
+    #     return True
+    # return False
+    return (s.lower().count('p') == s.lower().count('y'))
