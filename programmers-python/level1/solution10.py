ab39637e59c016f41cfc68338e229a6541e7781b
ujin2021 <ujin2021@naver.com>
2020-08-31 18:17:01 +0900

    문자열 다루기 기본

--- programmers-python/level1/solution10.py
+++ programmers-python/level1/solution10.py
@@ -1,10 +1,14 @@
 # 문자열 다루기 기본 - 실패
 def solution(s) :
     try :
-        if(int(s)) : return True
+        if(int(s) > -1 and len(s) == 4 or len(s) == 6) : return True
+        else : return False
     except Exception :
         return False
-print(solution('a234'))
-print(solution('1234'))
-print(solution('0000'))
-print(solution('00-1'))
+
+# s.isdigit()
+        
+print(solution('a234')) # false
+print(solution('1234')) # true
+print(solution('0000')) # true
+print(solution('-1')) # false (숫자로만 이루어져 있어야 하므로)
