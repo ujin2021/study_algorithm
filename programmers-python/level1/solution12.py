27cfc353328a622d85047b8cd8318c41bf1f642d
ujin2021 <ujin2021@naver.com>
2020-08-31 18:37:46 +0900

    모의고사

--- programmers-python/level1/solution12.py
+++ programmers-python/level1/solution12.py
@@ -1,14 +1,38 @@
 # 모의고사 - 실패
 def solution(answers):
     score = {'0' : 0, '1' : 0, '2' : 0}
-    pattern = {'0' : 12345, '1' : 21232425, '2' : 3311224455}
+    pattern = {'0' : '12345', '1' : '21232425', '2' : '3311224455'}
     for i in range(3) :
-        answer = (str(pattern[str(i)]) * len(answers))[:len(answers)]
-        for ans in range(len(answer)) :
-            if answer[ans] == answers[ans] :
+        answer = (str(pattern[str(i)]) * len(answers))[0:len(answers)]
+        for j in range(len(answer)) :
+            if (int(answer[j]) == answers[j]) :
                 score[str(i)] = score[str(i)] + 1
 
-    for i in range(3) :
-        return sorted(score.keys(), key = lambda x : score[str(i)])
+    max_score = max(score.values())
+    return [int(x)+1 for x in score.keys() if score[x] == max_score]
 
-print(solution('12345'))
+print(solution([1, 2, 3, 4, 5]))
+print(solution([1, 3, 2, 4, 2]))
+
+''' 다른사람 풀이
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
+'''
