2e507c8dd5f0e562c90fc77ee037871247bf0395
ujin2021 <ujin2021@naver.com>
2020-11-29 23:34:25 +0900

    자카드 유사도

---
+++ programmers-python/kakao/solution5.py
@@ -0,0 +1,45 @@
+# 2018 KAKAO BLIND RECRUITMENT 뉴스 클러스터링
+# 자카드 유사도
+def makeSet(s) :
+    tmp_list = []
+    for i in range(len(s) - 1) :
+        tmp = s[i:i+2]
+        if(tmp.isalpha()) :
+            tmp_list.append(tmp)
+    return tmp_list
+
+def solution(str1, str2):
+    str1_list = makeSet(str1.lower())
+    str2_list = makeSet(str2.lower())
+
+    str1_set = set(str1_list)
+    str2_set = set(str2_list)
+    union = list(str1_set | str2_set)
+    inter = list(str1_set & str2_set)
+
+    union_score = 0 # 합집합(max)
+    inter_score = 0 # 교집합(min)
+
+    # print(f'str1_list : {str1_list}, str2_list : {str2_list}, union : {union}, inter : {inter}')
+    for i in union :
+        union_score += max(str1_list.count(i), str2_list.count(i))
+
+    for i in inter :
+        minimum = min(str1_list.count(i), str2_list.count(i))
+        if(minimum == 0) :
+            inter_score += 1
+        else :
+            inter_score += min(str1_list.count(i), str2_list.count(i))
+
+    if(union_score == 0) :
+        return 65536
+
+    similarity = inter_score / union_score
+    return int(similarity * 65536)
+
+print(solution('FRANCE', 'french'))
+print(solution('handshake', 'shake hands'))
+print(solution('aa1+aa2', 'AAAA12'))
+print(solution('E=M*C^2', 'e=m*c^2'))
+    
+    
