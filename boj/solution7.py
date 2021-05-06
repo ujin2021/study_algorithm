f1bc6c3750a6fff8875895d9824b5b653fd3467b
ujin2021 <ujin2021@naver.com>
2021-05-06 23:54:26 +0900

    18352 특정 거리의 도시 찾기

--- boj/solution7.py
+++ boj/solution7.py
@@ -1,29 +1,48 @@
 # boj 18352 특정 거리의 도시 찾기 - BFS
 
 from collections import deque
-
+import sys
 # 도시 갯수, 도로 갯수, 거리정보, 출발도시
-n, m, k, x = map(int, input().split())
+n, m, k, x = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(n + 1)]
+
 # 도로 정보 입력
-for _ in range(m) :
-    a, b = map(int, input().split())
+for line in range(m) :
+    a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
 
-q = deque([[x, 0]]) # 시작 노드와 시작노드 부터의 최단거리
-visited = [False] * (n + 1)
-check = False # q를 검사하는 동안 k와 같은 것이 없으면 False
+visited = [-1] * (n + 1)
+visited[x] = 0 # 출발지에서 출발지 까지는 0
 
+q = deque([x]) # 시작 노드와 시작노드 부터의 최단거리
 while q :
     now = q.popleft()
-    if(visited[now[0]] == False) : # 방문하지 않은 노드라면
-        visited[now[0]] = True # 방문 했다고 표시
-        if(now[1] == k) : # 최단거리가 k와 같으면
-            print(now[0]) # 프린트
-            check = True
-    near = graph[now[0]]
+    near = graph[now]
     for _ in near :
-        q.append([_, now[1] + 1]) # 최단거리를 1 증가하여 q에 추가
+        if(visited[_] == -1) :
+            q.append(_) # 최단거리를 1 증가하여 q에 추가
+            visited[_] = visited[now] + 1
 
-if(check == False) :
+
+length = len(visited)
+result = [i for i in range(length) if(visited[i] == k)]
+
+if(len(result) == 0) :
     print(-1)
+else :
+    for _ in result :
+        print(_)
+
+'''
+4 5 3 1
+1 2
+1 3
+2 3
+2 4
+4 1
+
+만약 visited를 초기화 시 False로 하면, visited[x] = 0 으로 설정한 곳에서 False로 인식하므로
+시작노드인 1이 다시 업데이트 된다. 따라서 정답이 1이 나오게됨(정답은 -1)
+'''
+
+    
